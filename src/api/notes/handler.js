f4fcc5f7edbf76a4addafef559040c22f269eede
hendrimardani <hendrimardani290301@gmail.com>
2025-07-30 20:30:05 +0700

    success add authentications

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -19,7 +19,7 @@ class NotesHandler {
       status: 'success',
       message: 'Catatan berhasil ditambahkan',
       data: {
-        note: {
+        noteId: {
           id,
         }
       },
